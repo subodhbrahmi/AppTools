a1bd43f736dc14e55821d7e6f25a90d88e4fbb89
Subodh - Subodh WorkMachine <work@work.com>
2014-05-19 14:17:37 -0500

    Initial Commit - ReadData

---
+++ ReadData-BPMRD/ReadData-BPMRD/ReadBpmrd.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace ReadData_BPMRD
+{
+    partial class ReadBpmrd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReadBpmrd));
+            this.button1 = new System.Windows.Forms.Button();
+            this.ConnStat = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(318, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(208, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CONNECT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ConnStat
+            // 
+            this.ConnStat.AutoSize = true;
+            this.ConnStat.Location = new System.Drawing.Point(41, 26);
+            this.ConnStat.Name = "ConnStat";
+            this.ConnStat.Size = new System.Drawing.Size(0, 13);
+            this.ConnStat.TabIndex = 1;
+            // 
+            // ReadBpmrd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 417);
+            this.Controls.Add(this.ConnStat);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ReadBpmrd";
+            this.Text = "Reading From Database";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label ConnStat;
+    }
+}
+
---
+++ ReadData-BPMRD/ReadData-BPMRD/ReadBpmrd.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadData_BPMRD
+{
+    public partial class ReadBpmrd : Form
+    {
+        public ReadBpmrd()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Data.OracleClient.OracleConnection bpmrdconn = new System.Data.OracleClient.OracleConnection();
+                bpmrdconn.ConnectionString = "Data Source=EDGDCMT.DB.ATT.COM;User ID=sb834g;Password=Arthur@80;Unicode=True";
+                bpmrdconn.Open();
+                ConnStat.Text = "Connected to BPMRD TEST !!!";
+            }
+            catch (Exception ex)
+            {
+                ConnStat.Text = "Connection Failed !!!";
+            }
+                
+        }
+    }
+}
