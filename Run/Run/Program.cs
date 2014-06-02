5dc53043c0451c0ba4fb85ebd43f4a7d711c3579
Subodh - DellLaptop <subodhdell@gmail.com>
2014-06-01 23:46:27 -0500

    Added ReverseByWord class

--- Run/Run/Program.cs
+++ Run/Run/Program.cs
@@ -12,11 +12,14 @@ namespace Run
         static void Main(string[] args)
         {
             string inputstr;
-            ReverseString rs = new ReverseString();
+            //ReverseString rs = new ReverseString();
+            ReverseByWord rs = new ReverseByWord();
             Console.Write("Input String = ");
             inputstr=Console.ReadLine();
+            rs.RevStrWrd(inputstr);
             //rs.print("testing");
-            Console.WriteLine("Reversed String = " + rs.RevStr(inputstr));
+            //Console.WriteLine("Reversed String = " + rs.RevStr(inputstr));
+            //Console.WriteLine("Reversed String = " + rs.RevStrWrd(inputstr));
             Console.ReadKey();
 
         }
