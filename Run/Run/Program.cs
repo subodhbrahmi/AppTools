6d5e685080f3e1d4991cf6918d8314cbe857cf8a
Subodh - Subodh WorkMachine <work@work.com>
2014-06-03 12:34:09 -0500

    Added RevStrbyIndex function using Index methods

--- Run/Run/Program.cs
+++ Run/Run/Program.cs
@@ -16,10 +16,11 @@ namespace Run
             ReverseByWord rs = new ReverseByWord();
             Console.Write("Input String = ");
             inputstr=Console.ReadLine();
-            rs.RevStrWrd(inputstr);
+            //rs.RevStrWrd(inputstr);
             //rs.print("testing");
             //Console.WriteLine("Reversed String = " + rs.RevStr(inputstr));
-            Console.WriteLine("Reversed String = " + rs.RevStrWrd(inputstr));
+            //Console.WriteLine("Reversed String = " + rs.RevStrWrd(inputstr));
+            Console.WriteLine("Reversed String = " + rs.RevStrbyIndex(inputstr));
             Console.ReadKey();
 
         }
--- Run/Run/ReverseByWord.cs
+++ Run/Run/ReverseByWord.cs
@@ -19,9 +19,35 @@ namespace Run
             //InpStr = string.Join(" ", TestStr.Split(' ').Reverse());
             OutStr=string.Join(" ",TestStr.Split(' ').Reverse());
             return OutStr;
-            //Console.WriteLine(InpStr);
+            //Console.WriteLine(InpStr);  
+        }
+
+        //Function using IndexOf and LastIndexOf string operations
+
+        public string RevStrbyIndex(string TestStr)
+        {
+            string chkSpace = " ";
+            int InpLen = TestStr.Length;
+            string tempStr="";
+            int index;
+            index = TestStr.LastIndexOf(chkSpace);
+            for (int i = InpLen; index != -1; i-=index)
+            {
+                tempStr = tempStr + " " + TestStr.Substring(index + 1, i-(index+1));
+                index=TestStr.LastIndexOf(chkSpace, index-1);
+                   /* if(TestStr[index].ToString() == chkSpace)
+                    {
+                        tempStr = tempStr + TestStr.Substring(index + 1, i);
+
+                    }*/
+              }
+            tempStr = tempStr + " " + TestStr.Substring(0, TestStr.IndexOf(chkSpace));
+            //OutStr=tempStr;
+        return tempStr;    
+        }
 
             
-        }
-    }
+     }
+
+
 }
