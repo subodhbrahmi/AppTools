169d55d762a8616ae07845e412e1840189953cb5
subodhbrahmi <subodh_brahmi@yahoo.co.in>
2014-11-13 23:51:13 -0600

    New DemoWebapi project added reference from ( http://www.codeproject.com/Tips/678138/MVC-using-Web-API-and-JQuery-to-GET-or-POST-Data )

---
+++ DemoWebapi/DemoWebapi/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DemoWebapi.Models;
+
+namespace DemoWebapi.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        PersonEntities db = new PersonEntities();
+        // GET api/values
+        public IEnumerable<Person> Get()
+        {
+            return db.Persons.ToList();
+            //return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public Person Get(int id)
+        {
+            return db.Persons.Find(id);
+            //return "value";
+        }
+
+        // POST api/values
+        public void Post(List<string> value) //Person obj
+        {
+            try
+            {
+                Person obj = new Person();
+                obj.Name = value[0];
+                obj.Address = value[1];
+                obj.DOB = Convert.ToDateTime(value[2]);
+                db.Persons.Add(obj);
+                db.SaveChanges();
+
+            }
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DemoWebapi/DemoWebapi/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace DemoWebapi.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public DateTime DOB { get; set; }
+    }
+    public class PersonEntities : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+
+    }
+}
