e58038933b90eec64b04d4a256e14f29b3524ff3
subodhbrahmi <subodh_brahmi@yahoo.co.in>
2014-11-16 14:16:48 -0600

    Fixed Controllers, Views and DB files updated.

---
+++ DemoWebapi/DemoWebapi/Controllers/PersonController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using DemoWebapi.Models;
+
+namespace DemoWebapi.Controllers
+{
+    public class PersonController : Controller
+    {
+        ValuesController db = new ValuesController();
+
+        public ActionResult Index()
+        {
+            var list = db.Get(); // Gets the list of records from web api
+            return View(list);
+        }
+
+        public ActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
